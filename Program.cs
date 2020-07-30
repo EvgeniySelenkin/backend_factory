f1c528901938dafbf0ba6c2816f7abcc193c84bb
Evgeniy Selenkin <evgeniy.selenkin@gmail.com>
2020-07-30 15:56:12 +0600

    correct functions of find, variable in get functions, delete goto

--- Program.cs
+++ Program.cs
@@ -27,7 +27,12 @@ namespace SelenkinEE
             Console.WriteLine($"Общий объем резервуаров: {totalVolume}");
             Console.WriteLine();
             Console.WriteLine("В какой коллекции вы хотите произвести поиск? (Заводы, Установки, Резервуары)");
-        M: string choise = Console.ReadLine().ToLower();
+            string choise = Console.ReadLine().ToLower();
+            while(choise!= "заводы" || choise!= "установки" || choise!= "резервуары")
+            {
+                Console.WriteLine("Похоже вы ошиблись при вводе, введите снова");
+                choise = Console.ReadLine().ToLower();
+            }
             if (choise == "заводы")
             {
                 Console.WriteLine("Введите название завода");
@@ -49,11 +54,6 @@ namespace SelenkinEE
                 Console.WriteLine("Информация по данному резервуару");
                 FindTank(tanks, Name).GetInformation();
             }
-            else
-            {
-                Console.WriteLine("Похоже вы ошиблись при вводе, введите снова");
-                goto M;
-            }
 
             Console.WriteLine("Выгружаем объекты в json файл");
             SerializerToJSON(factories, units, tanks);
@@ -70,17 +70,17 @@ namespace SelenkinEE
 
             for(int i = 1;i<7;i++)
             {
-                ICell cell1 = sheet.GetRow(i).GetCell(0);
-                cell1.SetCellType(CellType.Numeric);
-                ICell cell2 = sheet.GetRow(i).GetCell(1);
-                cell2.SetCellType(CellType.String);
-                ICell cell3 = sheet.GetRow(i).GetCell(2);
-                cell3.SetCellType(CellType.Numeric);
-                ICell cell4 = sheet.GetRow(i).GetCell(3);
-                cell4.SetCellType(CellType.Numeric);
-                ICell cell5 = sheet.GetRow(i).GetCell(4);
-                cell5.SetCellType(CellType.Numeric);
-                tanks[i-1] = new Tank((int)cell1.NumericCellValue, cell2.StringCellValue, (int)cell3.NumericCellValue, (int)cell4.NumericCellValue, (int)cell5.NumericCellValue);
+                ICell tankIdCell = sheet.GetRow(i).GetCell(0);
+                tankIdCell.SetCellType(CellType.Numeric);
+                ICell tankNameCell = sheet.GetRow(i).GetCell(1);
+                tankNameCell.SetCellType(CellType.String);
+                ICell tankVolumeCell = sheet.GetRow(i).GetCell(2);
+                tankVolumeCell.SetCellType(CellType.Numeric);
+                ICell tankMaxVolumeCell = sheet.GetRow(i).GetCell(3);
+                tankMaxVolumeCell.SetCellType(CellType.Numeric);
+                ICell tankUnitIdCell = sheet.GetRow(i).GetCell(4);
+                tankUnitIdCell.SetCellType(CellType.Numeric);
+                tanks[i-1] = new Tank((int)tankIdCell.NumericCellValue, tankNameCell.StringCellValue, (int)tankVolumeCell.NumericCellValue, (int)tankMaxVolumeCell.NumericCellValue, (int)tankUnitIdCell.NumericCellValue);
             }
             /*
             tanks[0] = new Tank(1, "Резервуар 1", 1500, 2000, 1);
@@ -101,13 +101,13 @@ namespace SelenkinEE
             var units = new Unit[3];
             for (int i = 1; i < 4; i++)
             {
-                ICell cell1 = sheet.GetRow(i).GetCell(0);
-                cell1.SetCellType(CellType.Numeric);
-                ICell cell2 = sheet.GetRow(i).GetCell(1);
-                cell2.SetCellType(CellType.String);
-                ICell cell3 = sheet.GetRow(i).GetCell(2);
-                cell3.SetCellType(CellType.Numeric);
-                units[i - 1] = new Unit((int)cell1.NumericCellValue, cell2.StringCellValue, (int)cell3.NumericCellValue);
+                ICell unitIdCell = sheet.GetRow(i).GetCell(0);
+                unitIdCell.SetCellType(CellType.Numeric);
+                ICell unitNameCell = sheet.GetRow(i).GetCell(1);
+                unitNameCell.SetCellType(CellType.String);
+                ICell unitFactoryIdCell = sheet.GetRow(i).GetCell(2);
+                unitFactoryIdCell.SetCellType(CellType.Numeric);
+                units[i - 1] = new Unit((int)unitIdCell.NumericCellValue, unitNameCell.StringCellValue, (int)unitFactoryIdCell.NumericCellValue);
             }
 
             /*units[0] = new Unit(1, "ГФУ-1", 1);
@@ -126,13 +126,13 @@ namespace SelenkinEE
             XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(0);
             for (int i = 1; i < 3; i++)
             {
-                ICell cell1 = sheet.GetRow(i).GetCell(0);
-                cell1.SetCellType(CellType.Numeric);
-                ICell cell2 = sheet.GetRow(i).GetCell(1);
-                cell2.SetCellType(CellType.String);
-                ICell cell3 = sheet.GetRow(i).GetCell(2);
-                cell3.SetCellType(CellType.String);
-                factories[i - 1] = new Factory((int)cell1.NumericCellValue, cell2.StringCellValue, cell3.StringCellValue);
+                ICell factoryIdCell = sheet.GetRow(i).GetCell(0);
+                factoryIdCell.SetCellType(CellType.Numeric);
+                ICell factoryNameCell = sheet.GetRow(i).GetCell(1);
+                factoryNameCell.SetCellType(CellType.String);
+                ICell factoryDescriptionCell = sheet.GetRow(i).GetCell(2);
+                factoryDescriptionCell.SetCellType(CellType.String);
+                factories[i - 1] = new Factory((int)factoryIdCell.NumericCellValue, factoryNameCell.StringCellValue, factoryDescriptionCell.StringCellValue);
             }
             /*factories[0] = new Factory(1, "МНПЗ", "Московский нефтеперерабатывающий завод");
             factories[1] = new Factory(2, "ОНПЗ", "Омский нефтеперерабатывающий завод");*/
@@ -154,8 +154,7 @@ namespace SelenkinEE
             var factory = new Factory();
             for (int i = 0; i < factories.Length; i++)
                 if (factories[i].Name == name)
-                    factory = factories[i];
-            return factory;
+                    return factories[i];
         }
 
         public static Unit FindUnit(Unit[] units, string name)
@@ -163,8 +162,7 @@ namespace SelenkinEE
             var unit = new Unit();
             for (int i = 0; i < units.Length; i++)
                 if (units[i].Name == name)
-                    unit = units[i];
-            return unit;
+                    return units[i];
         }
 
         public static Tank FindTank(Tank[] tanks, string name)
@@ -172,8 +170,7 @@ namespace SelenkinEE
             var tank = new Tank();
             for (int i = 0; i < tanks.Length; i++)
                 if (tanks[i].Name == name)
-                    tank = tanks[i];
-            return tank;
+                    return tanks[i];
         }
 
         public static void SerializerToJSON(Factory[] factories, Unit[] units, Tank[] tanks)
