6e52bc515638d62d4bff832d052041d260d5258b
Evgeniy Selenkin <evgeniy.selenkin@gmail.com>
2020-08-12 11:25:53 +0600

    Add Interface and GetUnit from SQL

---
+++ Lesson1/Interfaces/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    interface IDataService
+    {
+        IList<Unit> GetUnits();
+        Unit FindUnitByTank(Tank tank);
+        Factory FindFactoryByUnit(Unit unit);
+        Unit FindUnitByName(string unitName);
+
+    }
+}
---
+++ Lesson1/Services/DataServiceSql.cs
@@ -0,0 +1,44 @@
+﻿using Org.BouncyCastle.Crypto.Tls;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public class DataServiceSql : IDataService
+    {
+        private readonly string connectionString = @"Data Source=LAPTOP-8TVASHA4;Initial Catalog=Factories;Integrated Security=True";
+        private readonly IOService ioService;
+        public async Task<IList<Unit>> GetUnits()
+        {
+            var units = new List<Unit>();
+            await using var connection = new SqlConnection(connectionString);
+            try
+            {
+                await connection.OpenAsync();
+            }
+            catch(Exception ex)
+            {
+                ioService.Exeption(ex);
+                return units;
+            }
+
+            await using var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Unit;";
+            await using var reader = await command.ExecuteReaderAsync();
+            while(reader.Read())
+            {
+                units.Add(new Unit 
+                { Id = (int)reader.GetFieldValue<object>(0), 
+                    Name = reader.GetFieldValue<object>(1).ToString(), 
+                    FactoryId = (int)reader.GetFieldValue<object>(2)
+                });
+            }
+            return units;
+        }
+        
+        
+
+    }
+}
